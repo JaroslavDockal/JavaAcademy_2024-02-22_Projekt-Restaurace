569e6223f58c27814f87bd5c552a2a1f55694d9e
Jaroslav Dockal <jarin.doc@gmail.com>
2024-03-04 18:22:42 +0100

    Prepared class for Dish

--- src/com/engeto/ja/restaurant/Dish.java
+++ src/com/engeto/ja/restaurant/Dish.java
@@ -1,16 +1,10 @@
-//Zásobník receptů
-//Kuchaři mají připraven zásobník jídel (dish + cook book).
+//Vytvoř třídu Dish, která bude reprezentovat jednotlivá jídla v naší restauraci.
 //U každého jídla/receptu ukládáme název (title), cenu (price), přibližnou dobu přípravy (preparation time) v minutách a URL odkaz na fotografii (image).
-// Fotografie samotná je na serveru, její ukládání tedy neřešíš.
+//Fotografie samotná je na serveru, její ukládání tedy neřešíš.
 //Ulož pouze název souboru bez přípony — například bolonske-spagety-01. (Všechny obrázky jsou ve formátu .PNG, ani příponu tedy nemusíme ukládat.)
-//
-//Doba přípravy musí být kladné číslo — systém nepovolí zadat záporné číslo či nulu.
-//
-//Kuchaři mají možnost některá jídla ze zásobníku vyřadit, přidat, nebo upravit.
-//V systému kvůli budoucímu zobrazování nesmí být jídlo/recept bez fotografie, ale na serveru je speciální
-//fotografie s názvem blank, kterou použij jako výchozí pro recepty, které zatím fotografii nemají.
+//Pokud nebude zadána fotografie, použij výchozí obrázek (například blank).
 
-package comm.engeto.restaurant;
+package com.engeto.ja.restaurant;
 
 import java.math.BigDecimal;
 
@@ -20,14 +14,50 @@ public class Dish {
     private int preparationTime;
     private String image;
 
-    public Dish(String title, BigDecimal price, int preparationTime, String image) {
+    public Dish(String title, BigDecimal price, int preparationTime, String image) throws RestaurantException {
         this.title = title;
         this.price = price;
-        this.preparationTime = preparationTime;
+        setPreparationTime(preparationTime);
         this.image = image;
     }
 
-    public Dish(String title, BigDecimal price, int preparationTime) {
-        this(title, price, preparationTime, )
+    public Dish(String title, BigDecimal price, int preparationTime) throws RestaurantException {
+        this(title, price, preparationTime, Settings.getDefaultImage());
+    }
+
+    public void setPreparationTime(int preparationTime) throws RestaurantException{
+        if (preparationTime < 1) {
+            throw new RestaurantException("Zadaná chybná doba přípravy. Zadaná doba: \"" + preparationTime + "\" minut.");
+        }
+        this.preparationTime = preparationTime;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public int getPreparationTime() {
+        return preparationTime;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    @Override
+    public String toString() {
+        return "Jídlo: " + title + ", cena: " + price + " Kč, doba přípravy: " + preparationTime + " minut, obrázek: " + image;
     }
 }
