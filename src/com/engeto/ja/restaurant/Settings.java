d9aa39afc2d3cd22960b5fcd32f808281d51b107
Jaroslav Dockal <jarin.doc@gmail.com>
2024-03-04 18:21:39 +0100

    Created class for project settings

--- src/com/engeto/ja/restaurant/Settings.java
+++ src/com/engeto/ja/restaurant/Settings.java
@@ -1,5 +1,26 @@
-package comm.engeto.restaurant;
+package com.engeto.ja.restaurant;
 
 public class Settings {
-    
+
+    private static final String DEFAULT_IMAGE = "blank";
+    private static final String DATE_FORMAT = "d.M. H:m";
+    private static final String DELIMITER = "\t";
+    private static final int MAX_TABLE_NUMBER = 19;
+
+    public static String getDefaultImage() {
+        return DEFAULT_IMAGE;
+    }
+
+    public static String getDelimiter() {
+        return DELIMITER;
+    }
+
+    public static String getDateFormat() {
+        return DATE_FORMAT;
+    }
+
+    public static int getMaxTableNumber() {
+        return MAX_TABLE_NUMBER;
+    }
+
 }
