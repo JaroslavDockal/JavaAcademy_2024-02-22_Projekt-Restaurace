829d931e941461bc86268ac7c2af4d6e5d83ae76
Jaroslav Dockal <jarin.doc@gmail.com>
2024-04-25 09:24:35 +0200

    Added input verification

--- src/com/engeto/ja/restaurant/Dish.java
+++ src/com/engeto/ja/restaurant/Dish.java
@@ -1,28 +1,33 @@
-//Vytvoř třídu Dish, která bude reprezentovat jednotlivá jídla v naší restauraci.
-//U každého jídla/receptu ukládáme název (title), cenu (price), přibližnou dobu přípravy (preparation time) v minutách a URL odkaz na fotografii (image).
-//Fotografie samotná je na serveru, její ukládání tedy neřešíš.
-//Ulož pouze název souboru bez přípony — například bolonske-spagety-01. (Všechny obrázky jsou ve formátu .PNG, ani příponu tedy nemusíme ukládat.)
-//Pokud nebude zadána fotografie, použij výchozí obrázek (například blank).
-
 package com.engeto.ja.restaurant;
 
 import java.math.BigDecimal;
 
 public class Dish {
-    private final String title;
+    private String title;
     private BigDecimal price;
     private int preparationTime;
     private String image;
 
     public Dish(String title, BigDecimal price, int preparationTime, String image) throws RestaurantException {
-        this.title = title;
-        this.price = price;
+        setTitle(title);
+        setPrice(price);
         setPreparationTime(preparationTime);
-        this.image = image;
+        setImage(image);
     }
 
     public Dish(String title, BigDecimal price, int preparationTime) throws RestaurantException {
         this(title, price, preparationTime, Settings.getDefaultImage());
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(BigDecimal price) throws RestaurantException  {
+        if (price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new RestaurantException("Zadaná chybná cena. Zadaná cena: \"" + price + "\" Kč.");
+        }
+        this.price = price;
     }
 
     public void setPreparationTime(int preparationTime) throws RestaurantException{
@@ -30,10 +35,6 @@ public class Dish {
             throw new RestaurantException("Zadaná chybná doba přípravy. Zadaná doba: \"" + preparationTime + "\" minut.");
         }
         this.preparationTime = preparationTime;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
     }
 
     public void setImage(String image) {
