9673f927834128e58ef5ff576544d0ca0117bf80
Jaroslav Dockal <jarin.doc@gmail.com>
2024-04-26 22:45:19 +0200

    Updated file name for table summaries

--- src/com/engeto/ja/restaurant/Settings.java
+++ src/com/engeto/ja/restaurant/Settings.java
@@ -4,12 +4,12 @@ public class Settings {
 
     private static final String DEFAULT_IMAGE = "blank";
     private static final String DATE_FORMAT = "d.M. H:m";
-A    private static final String TIME_FORMAT = "HH:mm";
+    private static final String TIME_FORMAT = "HH:mm";
     private static final String DELIMITER = "\t";
     private static final int MAX_TABLE_NUMBER = 19;
     private static final String FILE_NAME_ORDERS = "resources/orders.txt";
     private static final String FILE_NAME_COOKBOOK = "resources/cookbook.txt";
-    private static final String FILE_NAME_SUMMARY = "resources/summary.txt";
+    private static final String FILE_NAME_SUMMARY = "resources/summaryTable";
 
     public static String getDefaultImage() {
         return DEFAULT_IMAGE;
