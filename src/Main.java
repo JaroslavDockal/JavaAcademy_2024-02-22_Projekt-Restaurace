208b047fbb6fb384e817c379e8fe4c45a581c3c2
Nikola Heliskova <he.ni@seznam.cz>
2024-05-14 17:33:19 +0200

    Started with calling of some relevant methods

--- src/Main.java
+++ src/Main.java
@@ -10,8 +10,10 @@ public class Main {
         CookBook cookBook = new CookBook();
         Orders orders = new Orders();
 
+//  1. Nacteni stavu evidence z disku
        loadDataFromDisk(cookBook, orders);
 
+//  2. Priprava testovacich dat
         addDishToCookBook(cookBook,"Kuřecí řízek obalovaný 150 g", BigDecimal.valueOf(150), 10);
         addDishToCookBook(cookBook,"Hranolky 150 g", BigDecimal.valueOf(50), 10);
         addDishToCookBook(cookBook,"Pstruh na víně 200 g", BigDecimal.valueOf(180), 30);
@@ -37,14 +39,11 @@ public class Main {
 //        addDishToCookBook(cookBook, "Espresso", BigDecimal.valueOf(40), 5);
 //        addDishToCookBook(cookBook, "Cappuccino", BigDecimal.valueOf(50), 5);
 //        addDishToCookBook(cookBook, "Čaj", BigDecimal.valueOf(30), 5);
-//
-//        // For Nikol
 //        addDishToCookBook(cookBook, "Vegetariánská pizza", BigDecimal.valueOf(200), 20);
 //        addDishToCookBook(cookBook, "Vegetariánský burger", BigDecimal.valueOf(150), 15);
 //        addDishToCookBook(cookBook, "Salát s avokádem", BigDecimal.valueOf(130), 10);
 //        addDishToCookBook(cookBook, "Tofu s rýží", BigDecimal.valueOf(120), 15);
 //        addDishToCookBook(cookBook, "Vegetariánské lasagne", BigDecimal.valueOf(160), 30);
-
 
         createNewOrder(cookBook, orders, 15, 1, 2);
         createNewOrder(cookBook, orders, 15, 2, 2);
@@ -62,12 +61,13 @@ public class Main {
 //        createNewOrder(cookBook, orders, 6, 10, 3);
 
 
-
+//
         setOrderFulfilmentTime(orders.getOrders(), 2);
         createNewOrder(cookBook, orders, 2, 3, 1);
 
         orders.printOrders();
 
+//  3. Vypis celkove ceny konzumace pro stul c.15
         printTotalPriceForTable(orders, 15);
 
         saveDataToDisk(cookBook, orders);
@@ -140,6 +140,7 @@ public class Main {
         }
     }
 
+//  4. Vypis vsech udaju ziskanych z metod
     public static void printUnfinishedOrdersCount (Orders orders) {
         RestaurantManager restaurantManager = new RestaurantManager();
         System.out.println("Počet nevyřízených objednávek: " + restaurantManager.getUnfinishedOrdersCount(orders.getOrders()));
@@ -164,6 +165,7 @@ public class Main {
         }
     }
 
+//  5. Zmenena data - ulozeni na disk
     public static void saveDataToDisk(CookBook cookBook, Orders orders) {
         FileOperations fileOperations = new FileOperations();
         String fileNameOrders = Settings.getFileNameOrders();
@@ -190,6 +192,7 @@ public class Main {
         }
     }
 
+//  6. Opetovne nacteni dat z disku
     public static void loadDataFromDisk(CookBook cookBook, Orders orders) {
         FileOperations fileOperations = new FileOperations();
         String fileNameOrders = Settings.getFileNameOrders();
